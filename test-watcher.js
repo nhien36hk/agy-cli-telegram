e5e653139fa66746ceb7a6dc6dfebff8445c3d39
Đinh Hữu Nhiên <kingofpro1410@gmail.com>
2026-06-05 13:23:02 +0700

    chore: remove temporary test scripts

--- test-watcher.js
+++
@@ -1,4 +0,0 @@
-const watcher = require('./src/core/watcher');
-console.log("Latest dir:", watcher.getLatestConversationDir());
-console.log("Current active tool:", watcher.getCurrentActiveTool());
-console.log("Latest turn:", watcher.getLatestTurnFromTranscript());
